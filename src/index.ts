89f5ca5ac7dc197ebf38cc449e60ed4c01efa2be
cody <cevek@ya.ru>
2019-03-02 19:43:08 +0500

    Add graphqlFactory

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,34 @@ type MakeNeverNonExistsKeys<T, Entity> = {
         : (P extends '__args' ? {} : "Property doesn't exist")
 };
 
-export type GraphQLJSONConstraint<T, Entity> = MakeNeverNonExistsKeys<T, Entity>;
+export type GraphQLJSONConstraint<T, Entity> = MakeNeverNonExistsKeys<T, NonNullable<Entity>>;
 
 export function maybe<T>(val: T): T | undefined {
     return val;
 }
+
+type IsUndefined<T> = undefined extends T ? undefined : never;
+export type TransformMethods<T> = {
+    [K in keyof T]: T[K] extends (args: infer Args) => infer Entity
+        ? <Query extends GraphQLJSONConstraint<Query, Entity>>(args: Args, query: Query | Entity) => Query | IsUndefined<Entity>
+        : never
+};
+
+export function graphqlFactory<Root extends object>(fetchGraphqlQuery: (query: string) => {}): TransformMethods<Root> {
+    const obj = {} as Root;
+    return new Proxy(obj, {
+        get(target: any, prop) {
+            const fn = target[prop];
+            if (fn) return fn;
+            target[prop] = (query: {}) => {
+                const queryS = toGraphQLQuery({[prop]: query});
+                return (fetchGraphqlQuery(queryS) as any)[prop];
+            };
+            return target[prop];
+        },
+    });
+}
+
 export function toGraphQLQuery(query: any): string {
     let s = '';
     if (typeof query === 'object' && query !== null) {

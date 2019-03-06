ddaee8f11954fe4dd405239621ca918b6bd31b0d
cody <cevek@ya.ru>
2019-03-07 01:54:12 +0500

    Fix finding unused props

--- common/findUnusedProps.ts
+++ common/findUnusedProps.ts
@@ -16,6 +16,8 @@ declare module 'typescript' {
     }
 }
 
+const QueryType = 'Query';
+
 export function findUnusedProps(program: ts.Program, files: ts.SourceFile[]) {
     const checker = program.getTypeChecker();
     const res: ts.Identifier[] = [];
@@ -31,7 +33,8 @@ export function findUnusedProps(program: ts.Program, files: ts.SourceFile[]) {
                         signature.declaration &&
                         ts.isFunctionLike(signature.declaration) &&
                         signature.declaration.typeParameters &&
-                        signature.declaration.typeParameters.length === 1
+                        signature.declaration.typeParameters.length === 1 &&
+                        signature.declaration.typeParameters[0].getText() === QueryType
                     ) {
                         handleObject(arg);
                     }
@@ -40,10 +43,40 @@ export function findUnusedProps(program: ts.Program, files: ts.SourceFile[]) {
         }
         ts.forEachChild(node, visitor);
 
+        function handleRefs(refSymbols: ts.ReferencedSymbol[], propName: ts.Identifier, propPos: number) {
+            if (refSymbols.length === 1 && refSymbols[0].references.length === 1) return;
+            const allRefs: ts.ReferenceEntry[] = [];
+            const hasUsage = refSymbols.some(refSymbol => {
+                const filteredRefs = refSymbol.references.filter(ref => {
+                    if (ref.fileName.indexOf('node_modules') > -1) return false;
+                    if (ref.textSpan.start === propPos) return false;
+                    return true;
+                });
+                allRefs.push(...filteredRefs);
+                return filteredRefs.some(ref => {
+                    const sourceFile = program.getSourceFile(ref.fileName);
+                    if (sourceFile) {
+                        const token = ts.getTokenAtPosition(sourceFile, ref.textSpan.start);
+                        return (
+                            token &&
+                            token.parent &&
+                            ts.isIdentifier(token) &&
+                            ts.isPropertyAccessExpression(token.parent)
+                        );
+                    }
+                    return false;
+                });
+            });
+            if (!hasUsage && allRefs.length > 0) {
+                if (res.includes(propName)) return;
+                res.push(propName);
+            }
+        }
         function handleObject(obj: ts.ObjectLiteralExpression) {
             const nodeSourceFile = node.getSourceFile();
             obj.properties.forEach(prop => {
                 if (!prop.name) return;
+                if (!ts.isPropertyAssignment(prop)) return;
                 if (!ts.isIdentifier(prop.name)) return;
                 const refSymbols = ts.FindAllReferences.findReferencedSymbols(
                     program,
@@ -53,27 +86,14 @@ export function findUnusedProps(program: ts.Program, files: ts.SourceFile[]) {
                     prop.name.getStart(),
                 );
                 if (refSymbols) {
-                    const hasUsage = refSymbols.some(refSymbol => {
-                        return refSymbol.references.some(ref => {
-                            // if (ref.isDefinition || ref.isInString) return;
-                            if (ref.textSpan.start === prop.pos) return false;
-                            const sourceFile = program.getSourceFile(ref.fileName);
-                            if (sourceFile) {
-                                const token = ts.getTokenAtPosition(sourceFile, ref.textSpan.start);
-                                return (
-                                    token &&
-                                    token.parent &&
-                                    ts.isIdentifier(token) &&
-                                    ts.isPropertyAccessExpression(token.parent)
-                                );
-                            }
-                            return false;
-                        });
-                    });
-                    if (!hasUsage) {
-                        if (res.includes(prop.name)) return;
-                        res.push(prop.name);
-                    }
+                    handleRefs(refSymbols, prop.name, prop.pos);
+                }
+                let initializer = prop.initializer;
+                if (ts.isArrayLiteralExpression(prop.initializer)) {
+                    initializer = prop.initializer.elements[0];
+                }
+                if (initializer && ts.isObjectLiteralExpression(initializer)) {
+                    handleObject(initializer);
                 }
             });
         }
